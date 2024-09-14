a5821ef7a5aab80bca0aaeb104bbb32d08aa848c
ray.lhr <lhr8457205@163.com>
2024-09-13 21:22:35 -0400

    add: framework setted

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type {Config} from 'tailwindcss';
+
+export default {
+    content: [
+      'src/**/*.{tsx,jsx,js,ts,html}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  } satisfies Config;
